a4499a199a0ff299f61b487fc4a9472f52089235
Jacob Roman <jcbrmn06@gmail.com>
2021-09-04 11:27:04 -0400

    got background to work

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -5,12 +5,3 @@
  */
 
 // You can delete this file if you're not using it
-exports.onRenderBody = ({
-    // ...
-    setBodyAttributes,
-}) => {
-    // ...
-    setBodyAttributes({
-        className: 'bg-gray-800',
-    });
-};
